e23a53d1ae486d361c8a97aec390f4a73f41f010
Eric Clemmons <eric@smarterspam.com>
2019-02-23 19:26:15 -0800

    Working build

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,8 @@
+import { Recorder } from "back-to-the-fixture";
 import { Request, Response } from "express";
 import * as graphql from "express-graphql";
 
 import { schema } from "./schema";
-import { Recorder } from "../src";
 
 export default graphql((req: Request, res: Response) => {
   const { mode } = req.query;
