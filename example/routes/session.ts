0336ab2398844d657cbf0631dde37b45e1b0b46a
Eric Clemmons <eric@smarterspam.com>
2019-03-15 13:10:38 -0700

    Move example to example/ folder

---
+++ example/routes/session.ts
@@ -0,0 +1,21 @@
+import * as express from "express";
+import * as session from "express-session";
+
+export default express()
+  .use(
+    session({
+      name: "back-to-the-fixture-example",
+      resave: false,
+      saveUninitialized: false,
+      secret: "secret"
+    })
+  )
+  .use((req, res, next) => {
+    const cookie = req.get("cookie");
+
+    if (!cookie && req.path !== "/login") {
+      return res.status(403).send(`<a href="/login">Login</a>`);
+    }
+
+    next();
+  });
