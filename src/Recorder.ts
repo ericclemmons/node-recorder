680fe216bf528d300c4bb22f7a5f397c47b34be3
Eric Clemmons <eric@smarterspam.com>
2019-03-28 22:34:04 -0700

    Working ignore/record/replay/rerecord!

--- src/Recorder.ts
+++ src/Recorder.ts
@@ -1,8 +1,15 @@
+import * as fs from "fs";
 import * as http from "http";
 import * as https from "https";
+import * as mkdirp from "mkdirp";
 import * as nock from "nock";
+import * as path from "path";
+import * as zlib from "zlib";
+import * as url from "url";
 
 import { Mode } from "./Mode";
+
+const fnv1a = require("@sindresorhus/fnv1a");
 
 const REQUEST_ARGUMENTS = new WeakMap();
 
@@ -13,19 +20,36 @@ enum Methods {
   POST = "POST"
 }
 
-interface Headers {
+// A more relaxed version of http.ReuestOptions
+interface Options {
   [key: string]: string;
+}
+
+interface RequestFixture {
+  body: string;
+  href: string;
+  method: Methods;
+  headers: http.IncomingHttpHeaders;
+  // url: UrlWithParsedQuery;
+}
+
+interface ResponseFixture {
+  body: object | string;
+  statusCode: number;
+  headers: http.IncomingHttpHeaders;
+}
+
+interface Fixture {
+  request: RequestFixture;
+  response: ResponseFixture;
 }
 
 interface InterceptedRequest {
   body: string;
-  headers: {
-    [key: string]: string;
-  };
+  headers: http.IncomingHttpHeaders;
   method: Methods;
-  options: {
-    [key: string]: string;
-  }; // http.RequestOptions;
+  options: Options;
+  req: http.ClientRequest;
   respond: nock.ReplyCallback;
 }
 
@@ -39,6 +63,43 @@ export class Recorder {
     this.patchNock();
   }
 
+  getFixture = (interceptedRequest: InterceptedRequest): Fixture => {
+    const { request } = this.normalize(interceptedRequest);
+    const fixturePath = this.getFixturePath(request);
+
+    return JSON.parse(fs.readFileSync(fixturePath, "utf8"));
+  };
+
+  getFixturePath(request: RequestFixture): string {
+    const { href } = request;
+    const { hostname, pathname } = url.parse(request.href);
+
+    if (!hostname) {
+      console.error(request);
+      throw new Error(
+        `Cannot parse hostname from fixture's "href": ${JSON.stringify(href)}`
+      );
+    }
+
+    if (!pathname) {
+      console.error(request);
+      throw new Error(
+        `Cannot parse pathname from fixture's "href": ${JSON.stringify(href)}`
+      );
+    }
+
+    const hash = fnv1a(JSON.stringify(request));
+    const fixturePath = path.join(
+      process.cwd(),
+      "__fixtures__",
+      hostname,
+      pathname,
+      `${hash}.json`
+    );
+
+    return fixturePath;
+  }
+
   handleRequest = (interceptedRequest: InterceptedRequest) => {
     const { mode } = this;
 
@@ -46,17 +107,47 @@ export class Recorder {
       case Mode.IGNORE:
         return this.ignoreRequest(interceptedRequest);
 
+      case Mode.RECORD:
+        return this.recordRequest(interceptedRequest);
+
+      case Mode.RERECORD:
+        return this.rerecordRequest(interceptedRequest);
+
+      case Mode.REPLAY:
+        return this.replayRequest(interceptedRequest);
+
       default:
         throw new Error(`Mode.${mode} is not supported`);
     }
+  };
+
+  handleResponse = (
+    interceptedRequest: InterceptedRequest,
+    fixture: Fixture
+  ) => {
+    const { respond } = interceptedRequest;
+    const { body, headers, statusCode } = fixture.response;
+
+    respond(null, [statusCode, body, headers]);
   };
 
   ignore() {
     this.mode = Mode.IGNORE;
   }
 
-  ignoreRequest = async (interceptedRequest: InterceptedRequest) => {
-    const { body, headers, method, options, respond } = interceptedRequest;
+  async ignoreRequest(interceptedRequest: InterceptedRequest) {
+    const { respond } = interceptedRequest;
+    const { body, headers, statusCode } = await this.makeRequest(
+      interceptedRequest
+    );
+
+    respond(null, [statusCode, body, headers]);
+  }
+
+  async makeRequest(
+    interceptedRequest: InterceptedRequest
+  ): Promise<ResponseFixture> {
+    const { body, headers, method, options } = interceptedRequest;
 
     const request = (options.proto === "https"
       ? this.httpsRequest
@@ -88,8 +179,23 @@ export class Recorder {
           headers["transfer-encoding"] === "chunked"
         ) {
           const decoded = Buffer.concat(chunks);
+          const unzipped = zlib.gunzipSync(decoded).toString("utf8");
 
-          resolve(decoded);
+          // TODO Is this the correct thing to do?
+          delete headers["content-encoding"];
+          delete headers["transfer-encoding"];
+
+          try {
+            const json = JSON.parse(unzipped);
+
+            // TODO Is this safe to assume?
+            headers["content-encoding"] = "application/json";
+            resolve(json);
+          } catch (error) {
+            resolve(unzipped);
+          }
+
+          resolve(unzipped);
         } else {
           resolve(Buffer.concat(chunks).toString("utf8"));
         }
@@ -98,22 +204,79 @@ export class Recorder {
       response.once("error", reject);
     });
 
-    respond(null, [response.statusCode, responseBody, response.headers]);
-  };
+    return {
+      statusCode: response.statusCode as number,
+      headers: response.headers,
+      body: responseBody
+    };
+  }
+
+  normalize(request: InterceptedRequest, response?: ResponseFixture) {
+    const { body, headers, method, options } = request;
+    const { href } = url.parse(options.href);
+
+    // TODO Allow `recorder.configure` to customize this:
+    // https://netflix.github.io/pollyjs/#/configuration?id=defaults
+    return {
+      request: { method, href: href as string, headers, body },
+      response
+    };
+  }
 
   record() {
     this.mode = Mode.RECORD;
+  }
+
+  async recordRequest(request: InterceptedRequest) {
+    const { respond } = request;
+
+    try {
+      const fixture = this.getFixture(request);
+      const { statusCode, body, headers } = fixture.response;
+      console.log("Replaying", fixture.request.href);
+
+      return respond(null, [statusCode, body, headers]);
+    } catch (error) {
+      // TODO Better messaging we're recording
+      console.error(error);
+    }
+
+    return this.rerecordRequest(request);
   }
 
   replay() {
     this.mode = Mode.REPLAY;
   }
 
+  async replayRequest(interceptedRequest: InterceptedRequest) {
+    const { req } = interceptedRequest;
+
+    try {
+      const fixture = await this.getFixture(interceptedRequest);
+
+      return this.handleResponse(interceptedRequest, fixture);
+    } catch (error) {
+      req.emit("error", error);
+    }
+  }
+
   rerecord() {
     this.mode = Mode.RERECORD;
   }
 
-  patchNock = () => {
+  async rerecordRequest(request: InterceptedRequest) {
+    const { respond } = request;
+    const response = await this.makeRequest(request);
+    const fixture = this.normalize(request, response) as Fixture;
+
+    this.saveFixture(fixture);
+
+    const { statusCode, body, headers } = fixture.response;
+
+    respond(null, [statusCode, body, headers]);
+  }
+
+  patchNock() {
     // This is Nock's `OverriddenClientRequest`
     const { ClientRequest } = http;
 
@@ -128,9 +291,18 @@ export class Recorder {
 
       return req;
     };
-  };
+  }
 
-  setupNock = () => {
+  saveFixture(fixture: Fixture) {
+    const fixturePath = this.getFixturePath(fixture.request);
+    const serialized = JSON.stringify(fixture, null, 2);
+
+    console.log("Saving", fixturePath);
+    mkdirp.sync(path.dirname(fixturePath));
+    fs.writeFileSync(fixturePath, serialized);
+  }
+
+  setupNock() {
     nock.cleanAll();
 
     const interceptor = nock(/.*/).persist();
@@ -150,6 +322,7 @@ export class Recorder {
             headers,
             method,
             options,
+            req,
             respond: respond as nock.ReplyCallback
           };
 
@@ -158,5 +331,5 @@ export class Recorder {
     });
 
     nock.activate();
-  };
+  }
 }
