c4f2f8d3a58ebfd800f8d90c69bf614659646e79
Eric Clemmons <eric@smarterspam.com>
2019-04-01 22:03:15 -0700

    Attempt to find/replace oauth tokens

--- recorder.config.js
+++ recorder.config.js
@@ -1,21 +1,43 @@
+const tokens = new Map();
+const mockTokens = new Map();
+
 module.exports = {
   normalizers: [
     function fakeOauthToken(request, response) {
-      const fakeToken = "FAKE_ACCESS_TOKEN";
-
       // Replace `?access_token=...`
-      if (request.url.query && request.url.query.access_token) {
-        request.url.query.access_token = fakeToken;
+      if (request.url.query.access_token) {
+        const { access_token } = request.url.query;
+        const mockToken = mockTokens.get(access_token);
+
+        if (mockToken) {
+          request.url.query.access_token = mockToken;
+        }
       }
 
       // Replace `Authorization: Bearer ...`
       if (request.headers.authorization) {
-        request.headers.authorization = `Bearer ${fakeToken}`;
+        const [, access_token] = request.headers.authorization.split(" ");
+        const mockToken = mockTokens.get(access_token);
+
+        if (mockToken) {
+          request.headers.authorization = `Bearer ${mockToken}`;
+        }
       }
 
-      // Replace `{ access_token: '...' }`
+      // Replace `/oauth/token`'s response:
+      //   { access_token: '...' }`
       if (response && response.body.access_token) {
-        response.body.access_token = fakeToken;
+        const { username } = request.url.query;
+        const { access_token } = request.body;
+        const mockToken = `${username}_access_token`;
+
+        // Associate username with real access token
+        tokens.set(username, access_token);
+        // Associate real access token fake one
+        mockTokens.set(access_token, mockToken);
+
+        // Replace tokens with username for
+        response.body.access_token = mockToken;
       }
     }
   ]
