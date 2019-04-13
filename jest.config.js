d4e2d528d119a762657d4f9612fe307dd5ddf5c7
Eric Clemmons <eric@smarterspam.com>
2019-04-12 22:28:02 -0700

    Use ts-node for testing jest-preset.ts

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,16 @@
+require("ts-node/register");
+
 const { merge } = require("lodash");
 
 module.exports = merge(
   {
     moduleNameMapper: {
-      "back-to-the-fixture": "<rootDir>/src"
+      "back-to-the-fixture": "<rootDir>/dist"
     },
     testEnvironment: "node"
   },
   require("ts-jest/jest-preset"),
 
   // @ts-ignore
-  require("./dist/jest-preset")
+  require("./src/jest-preset")
 );
